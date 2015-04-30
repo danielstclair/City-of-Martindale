46aee9dec7e8be8600905f24951432ad6dfc0cdb
Paul Kokoszyna <pkoko9@hotmail.com>
2015-04-30 12:10:47 -0500

    Updateing routes in controller not yt functioning

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,5 +7,59 @@ angular.module('app', ['app.controllers', 'ui.router'])
 		controller: 'HomeCtrl'
 	});
 
+		.state('events', {
+		url: '/events',
+		templateUrl: 'templates/events.html',
+		controller: 'EventsCtrl'
+	});
+
+		.state('about', {
+		url: '/about',
+		templateUrl: 'templates/about.html',
+		controller: 'AboutCtrl'
+	});
+
+		.state('lifestyle', {
+		url: '/lifestyle',
+		templateUrl: 'templates/lifestyle.html',
+		controller: 'LifestyleCtrl'
+	});
+
+		.state('churches', {
+		url: '/churches',
+		templateUrl: 'templates/churches.html',
+		controller: 'ChurchesCtrl'
+	});
+
+		.state('businesses', {
+		url: '/businesses',
+		templateUrl: 'templates/businesses.html',
+		controller: 'BusinessesCtrl'
+	});
+
+		.state('cityinfo', {
+		url: '/cityinfo',
+		templateUrl: 'templates/cityinfo.html',
+		controller: 'CityInfoCtrl'
+	});
+
+		.state('contact', {
+		url: '/contact',
+		templateUrl: 'templates/contact.html',
+		controller: 'ContactCtrl'
+	});
+
+		.state('map', {
+		url: '/map',
+		templateUrl: 'templates/map.html',
+		controller: 'MapCtrl'
+	});
+
+		.state('history', {
+		url: '/history',
+		templateUrl: 'templates/history.html',
+		controller: 'HistoryCtrl'
+	});
+
 	$urlRouterProvider.otherwise('/home');
 });
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,31 +1,9 @@
 angular.module('app.controllers', [])
 
-.controller('NavCtrl', function($scope){
-	$scope.navLinks = false;
-	$scope.menu = false;
-	$scope.hideNav = false;
-	$scope.aboutLinks = false;
-	$scope.cityInfo = false;
-	$scope.toggleNav = function(){
-		$scope.navLinks = !$scope.navLinks;
-		$scope.menu = !$scope.menu;
-		$scope.hideNav = !$scope.hideNav;
-	}
-	$scope.showAbout = function(){
-		$scope.aboutLinks = true;
-	}
-	$scope.toggleAbout = function(){
-		$scope.aboutLinks = !$scope.aboutLinks;
-	}
-	$scope.showCity = function(){
-		$scope.cityInfo = true;
-	}
-	$scope.toggleCity = function(){
-		$scope.cityInfo = !$scope.cityInfo;
-	}
-})
 .controller('HomeCtrl', function($scope) {
 
-	
+});
+
+.controller('EventsCtrl', function($scope) {
 
 });
